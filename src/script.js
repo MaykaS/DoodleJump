bf34463fa1043f52d1d4268b05300b7a4323ab8c
Maya Sagalin <mayasag10@gmail.com>
2024-07-27 13:53:07 +0300

    added fall, key controle, game over, more todos

--- src/script.js
+++ src/script.js
@@ -2,17 +2,27 @@
 const grid = document.querySelector('.grid');
 const doodler = document.createElement('div');
 let doodlerLeftSpace = 50;
-let doodlerBottomSpace =100;
+let startPoint = 100;
+let doodlerBottomSpace =startPoint;
 let platformCount = 5;
 let platforms = [];
 let isGameOver=false;
 let upTimerId;
+let downTimerId;
+let leftTimerId;
+let rightTimerId;
+let isJumping = true;
+let isGoingLeft = false;
+let isGoingRight = false;
+
 
 function createDoodler(){
     grid.appendChild(doodler);
     doodler.classList.add('doodler'); //style
+    doodlerLeftSpace = platforms[0].left; //doodler starts in correct space
     doodler.style.left = doodlerLeftSpace + 'px';
     doodler.style.bottom = doodlerBottomSpace + 'px';
+    console.log(doodler.style.bottom);
 }
 
 class Platform{
@@ -50,18 +60,88 @@ function movePlatforms(){
 }
 
 function jump(){
+    clearInterval(downTimerId); //stop falling
+    isJumping=true;
     upTimerId = setInterval(function(){
         doodlerBottomSpace +=20;
-        doodler.style.bottom = doodlerBottomSpace + 'px';
+        doodler.style.bottom = doodlerBottomSpace + 'px'; //shoot up
+        if(doodlerBottomSpace>startPoint+250){
+            fall();
+        }
+    },50);
+}
+
+function fall(){
+    clearInterval(upTimerId); //stop jumping
+    isJumping=false;
+    downTimerId = setInterval(function(){
+        doodlerBottomSpace -=5;
+        doodler.style.bottom = doodlerBottomSpace + 'px'; //fall down
+        if(doodlerBottomSpace<=0){
+            gameOver();
+        }
+        //if falls onto a platform - jump again // 60 - doodler size
+        platforms.forEach(platform =>{
+            if(doodlerBottomSpace>=platform.bottom && 
+                doodlerBottomSpace<=(platform.bottom+15) && 
+                (doodlerLeftSpace + 60) >=platform.left &&
+                doodlerLeftSpace <= (platform.left +85) && !isJumping){
+                    console.log("Pew Landed");
+                    startPoint = doodlerBottomSpace; //override 
+                    jump();
+            }
+        })
+        
+    },30);
+}
+
+function gameOver(){
+    console.log("WaWa Game Over!");
+    isGameOver=true;
+    clearInterval(upTimerId);
+    clearInterval(downTimerId);
+}
+
+function control(event){
+    if(event.key ==="ArrowLeft"){
+        moveLeft();
+    }
+    else if(event.key === "ArrowRight"){
+        moveRight();
+    }
+}
+
+function moveLeft(){
+    clearInterval(rightTimerId);
+    isGoingRight=false;
+    isGoingLeft=true;
+    leftTimerId = setInterval(function(){
+        if(doodlerLeftSpace>=0){
+            doodlerLeftSpace-=5;
+            doodler.style.left = doodlerLeftSpace+'px';
+        }else moveRight();
+    },30);
+}
+
+function moveRight(){
+    clearInterval(leftTimerId);
+    isGoingLeft=false;
+    isGoingRight=true;
+    rightTimerId=setInterval(function(){
+        if(doodlerLeftSpace<=(400-60)){ //grid.width - doodler.width
+            doodlerLeftSpace+=5;
+            doodler.style.left=doodlerLeftSpace+'px';
+        }else moveLeft();
     },30);
 }
 
 function start(){
     if(!isGameOver){
-        createDoodler();
         createPlatforms();
+        createDoodler();
         setInterval(movePlatforms,30); //constantly move
         jump();
+        document.addEventListener('keyup',control); //key control
     } 
 
 }
@@ -69,11 +149,17 @@ start(); //attach to button
 
 /**
  * Add-ons = 
- * 1. button to start
- * 2. platforms within the grid 
- * 3. doodle jump
- * 4. doodle fall 
- * 5. game over
- * 6. score
- * 7. always adding platforms 
+ *  button to start
+ *  platforms within the grid 
+ *  (V) doodle jump
+ *  (V) doodle fall  
+ *  (V) if fall on latform jump again  
+ *  (V) game over
+ *  score
+ *  always adding platforms 
+ *  (V) start on platform
+ *  (V) controle left
+ *  (V) controle right
+ * remove hardocoded things
+ *  
  */
